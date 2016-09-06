23e6f7f25770b124af5aafcb802b9f0bab328d17
Alexis Reigel <mail@koffeinfrei.org>
2016-09-06 21:18:37 +0200

    CORS is apparently not needed

--- www/js/setup.js
+++ www/js/setup.js
@@ -1,11 +1,3 @@
-// setup CORS
-$.ajaxSetup({
-  xhrFields: {
-    withCredentials: true
-  },
-  crossDomain: true
-});
-
 // make all urls absolute
 $.ajaxPrefilter(function(options) {
   options.url = 'http://192.168.1.7:3000' + options.url;
