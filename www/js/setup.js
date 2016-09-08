ddd5c8f0cc3566857b726653b205ca10132215b6
Alexis Reigel <mail@koffeinfrei.org>
2016-09-08 11:18:49 +0200

    set production server url

--- www/js/setup.js
+++ www/js/setup.js
@@ -1,4 +1,4 @@
 // make all urls absolute
 $.ajaxPrefilter(function(options) {
-  options.url = 'http://192.168.1.7:3000' + options.url;
+  options.url = 'https://app.mykonote.com' + options.url;
 });
